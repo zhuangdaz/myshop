8536fe2904db5c74a5e1315b3e73c0761de78bfe
Zhuangda Zhu <zhuangdaz@gmail.com>
2019-01-23 22:57:16 -0800

    Add celery to support email notification

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cart.cart import Cart
+from .forms import OrderCreateForm
+from .models import OrderItem
+from .tasks import order_created
+
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         quantity=item['quantity'],
+                                         price=item['price'])
+            cart.clear()
+            order_created.delay(order.id)
+            return render(request, 'orders/order/created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'form': form, 'cart': cart})
