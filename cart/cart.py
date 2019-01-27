b87fb3671f0229ee712785b7c7a16fcea727635d
Zhuangda Zhu <zhuangdaz@gmail.com>
2019-01-27 13:02:58 -0800

    Apply coupon

--- cart/cart.py
+++ cart/cart.py
@@ -3,6 +3,7 @@ from decimal import Decimal
 from django.conf import settings
 
 from shop.models import Product
+from coupons.models import Coupon
 
 
 class Cart(object):
@@ -16,6 +17,7 @@ class Cart(object):
         if not cart:
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+        self.coupon_id = self.session.get('coupon_id')
 
     def add(self, product, quantity=1, update_quantity=False):
         product_id = str(product.id)
@@ -61,3 +63,17 @@ class Cart(object):
     def clear(self):
         del self.session[settings.CART_SESSION_ID]
         self.save()
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            return Coupon.objects.get(id=self.coupon_id)
+        return None
+
+    def get_discount(self):
+        if self.coupon:
+            return round((self.coupon.discount / Decimal('100')) * self.get_total_price(), 2)
+        return Decimal('0')
+
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()
--- coupons/models.py
+++ coupons/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
 
 class Coupon(models.Model):
     code = models.CharField(max_length=50,
